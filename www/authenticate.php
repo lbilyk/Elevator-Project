3f5020d9b8ff67ab4a467a2aa0c2027890cf0e21
Lu <lbilyk8018@conestogac.on.ca>
2019-07-08 15:43:49 -0400

    Fix: Fixed authentication issue
    Fixed missing database check for authentication

--- www/authenticate.php
+++ www/authenticate.php
@@ -1,10 +1,17 @@
 <?php
-
+require_once 'server/dbconfig.php';
 session_start();
 $authError = false;
 $username = $_POST['username'];
 $password = $_POST['password'];
 if ($username && $password) {
+    global $mysqli;
+    $query = "SELECT * FROM users WHERE username = ?";
+    $statement = $mysqli->prepare($query);
+    $statement->bind_param('s', $username);
+    $statement->execute();
+    $result = $statement->get_result();
+    $user = $result->fetch_assoc();
     if ($user["username"] == $username && $user["password"] == $password) {
         $authenticated = true;
         $_SESSION['user'] = $user["name"];
